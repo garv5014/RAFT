f011c70546d4927447e48e2729ce5f1ebbcd8abf
garv5014 <garv5014@gmail.com>
2024-02-24 11:46:50 -0700

    RAFT no tests

--- RAFT/Program.cs
+++ RAFT/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RAFT;
+
+foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.log"))
+{
+    File.Delete(file);
+}
+
+List<RaftNode> nodes = new List<RaftNode>();
+for (int i = 0; i < 3; i++)
+{
+    nodes.Add(new RaftNode(nodes));
+}
+
+foreach (var node in nodes)
+{
+    node.Initialize();
+}
+
+Console.WriteLine("Election running. Press key to stop election run.");
+Console.ReadKey();
--- RAFT/RaftNode.cs
+++ RAFT/RaftNode.cs
@@ -1,38 +1,198 @@
 ﻿namespace RAFT;
 
-// 
-
-
 public class RaftNode
 {
-    public RaftNodeState State { get; set; }
-    public int Term { get; set; }
-    public Guid VotedFor { get; set; }
+    private RaftNodeState State = RaftNodeState.Follower;
+
+    private int Term;
+    private Guid VotedFor { get; set; }
     public Guid Name { get; set; }
 
-    public System.Timers.Timer ElectionTimer { get; set; }
-    public RaftNode()
+    private Random Random = new Random();
+
+    private List<RaftNode> OtherNodes { get; set; }
+
+    private string FilePath;
+
+    private int ElectionTimeout;
+
+    private DateTime LastHeartbeat;
+
+    private bool IsAlive;
+
+    public RaftNode(List<RaftNode> otherNodes)
     {
         Term = 0;
         VotedFor = Guid.Empty;
         State = RaftNodeState.Follower;
         Name = Guid.NewGuid();
-        var ran = new Random();
-        ElectionTimer = new System.Timers.Timer(ran.Next(150, 300));
+        FilePath = $"{Name}.log";
+        OtherNodes = otherNodes;
+        UpdateElectionTimer();
+        IsAlive = false;
     }
 
-    public void StartElection()
+    private void UpdateElectionTimer()
+    {
+        ElectionTimeout = Random.Next(150, 300);
+        LastHeartbeat = DateTime.UtcNow;
+    }
+
+    public void Initialize()
+    {
+        IsAlive = true;
+        MakeTimeoutThread();
+    }
+
+    // Receive vote request return bool if vote is granted
+    public bool ReceiveVoteRequest(int term, Guid candidateId)
+    {
+        if (!IsAlive)
+        {
+            return false;
+        }
+
+        if (term > Term || ((VotedFor == Guid.Empty || VotedFor == candidateId) && term == Term))
+        {
+            Term = term;
+            VotedFor = candidateId;
+            State = RaftNodeState.Follower;
+            WriteToLog($"Voted for {candidateId} the term is {term}");
+            return true;
+        }
+        WriteToLog($"Did not vote for {candidateId} in term {term}");
+        return false;
+    }
+
+    // Receive a heartbeat from a leader
+    public void ReceiveHeartbeat(int term, Guid leaderId)
+    {
+        if (term >= Term)
+        {
+            Term = term;
+            State = RaftNodeState.Follower;
+            LastHeartbeat = DateTime.UtcNow;
+            WriteToLog($"Received heartbeat from {leaderId}");
+        }
+    }
+
+    // Keep track of time out and start election if needed or send heart beats if leader
+    public void Update()
+    {
+        if (!IsAlive)
+        {
+            return;
+        }
+
+        if (DateTime.UtcNow - LastHeartbeat > TimeSpan.FromMilliseconds(ElectionTimeout))
+        {
+            StartElection();
+        }
+        else if (State == RaftNodeState.Leader)
+        {
+            SendHeartbeats();
+        }
+    }
+
+    public void MakeTimeoutThread()
+    {
+        new Thread(() =>
+        {
+            while (IsAlive)
+            {
+                if (State == RaftNodeState.Leader)
+                {
+                    Task.Delay(100).Wait();
+                    SendHeartbeats();
+                }
+                else if (ElectionTimedOut())
+                {
+                    WriteToLog($"Node {Name} timed out");
+                    StartElection();
+                }
+            }
+        }).Start();
+    }
+
+    private void StartElection()
     {
         State = RaftNodeState.Candidate;
         Term++;
         VotedFor = Name;
-        ElectionTimer.Start();
+        WriteToLog($"Node {Name} started election for term {Term}");
+        UpdateElectionTimer();
+        int votes = 1;
+        foreach (var node in OtherNodes)
+        {
+            if (node.Name != Name)
+            {
+                new Thread(() =>
+                {
+                    if (node.ReceiveVoteRequest(Term, Name))
+                    {
+                        votes++;
+                    }
+                    else
+                    {
+                        WriteToLog($"Node {node.Name} did not vote for {Name} in term {Term}");
+                    }
+                }).Start();
+            }
+        }
+        if (votes > OtherNodes.Count / 2)
+        {
+            State = RaftNodeState.Leader;
+            WriteToLog($"Node {Name} became leader for term {Term}");
+            SendHeartbeats();
+        }
+        else
+        {
+            State = RaftNodeState.Follower;
+        }
     }
 
-    public void IncreaseTimer()
+    private bool ElectionTimedOut()
     {
-        var ran = new Random();
-        ElectionTimer.Interval = ran.Next(150, 300);
+        return DateTime.UtcNow - LastHeartbeat > TimeSpan.FromMilliseconds(ElectionTimeout);
+    }
+
+    // send heartbeats to all other nodes
+    private void SendHeartbeats()
+    {
+        if (!IsAlive)
+        {
+            return;
+        }
+        foreach (var node in OtherNodes)
+        {
+            if (node.Name != Name)
+            {
+                node.ReceiveHeartbeat(Term, Name);
+            }
+        }
+    }
+
+
+
+    private void WriteToLog(string entry)
+    {
+        using (var stream = new FileStream(FilePath, FileMode.Append, FileAccess.Write, FileShare.Write))
+        using (var writer = new StreamWriter(stream))
+        {
+            writer.WriteLine($"{DateTime.UtcNow}:{entry}");
+        }
+    }
+
+    // Function that "Kills" the node for testing
+    public void KillNode()
+    {
+        IsAlive = false;
+    }
+
+    // Function that "Revives" the node for testing
+    public void ReviveNode()
+    {
+        IsAlive = true;
     }
 }
 
